53a1fb1c8243f8518ab4dcb1ca30cd9f1cfd125e
artha <artha.djoe@gmail.com>
2021-06-15 14:35:57 +0200

    final commit

--- jour-2/exercice8.js
+++ jour-2/exercice8.js
@@ -1,6 +1,6 @@
 var limit = 50
-var score = 64
-var password = "azerty"
+var score = 3
+var password = "aze"
 if ( password.length > 5 && score >= limit ) {
     console.log("Everything is good")
 } else if ( password.length > 5 || score >= limit ) {
