fd2e8363dbb2f4acf56c5eaf7d5fb1c7cbb9ea56
artha <artha.djoe@gmail.com>
2021-06-15 12:18:32 +0200

    initial commit

---
+++ jour01.js
@@ -0,0 +1,45 @@
+console.log("hello world")
+
+var test = "My name is Artha"
+
+console.log(test)
+
+var name = "Artha"
+
+console.log("nice to meet you " + name)
+
+var testLength = "I'm very long !"
+
+console.log(testLength.length)
+
+var food = "croissant is meh"
+
+food = food.replace("meh", "so good")
+
+console.log(food)
+
+var basic = "This is Cool"
+
+var basicUp = basic.toUpperCase()
+
+var basicDown = basic.toLowerCase()
+
+console.log(basic)
+console.log(basicUp)
+console.log(basicDown)
+
+var word = "banana"
+var letters = word.split('')
+
+console.log(letters)
+
+var age = 31
+var template = (`i'm ${age} years old`)
+
+console.log(template)
+
+var firstName = 'Artha'
+var lastName = "Djoehana"
+var fullName = firstName + " " + lastName
+
+console.log(fullName)
