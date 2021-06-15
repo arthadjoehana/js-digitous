c130fea0939711dfa3473d14834696e80726a14c
artha <artha.djoe@gmail.com>
2021-06-15 14:24:03 +0200

    added bonus

---
+++ jour-2/bonus.js
@@ -0,0 +1,6 @@
+var random = Math.floor(Math.random() * 6) + 1;
+if ( random === 6 ) {
+    console.log("Yes I win !")
+} else {
+    console.log("So close...")
+}
---
+++ jour-2/bonus2.js
@@ -0,0 +1,41 @@
+var month = "January"
+switch (month) {
+	case "January": 
+		console.log("Winter");
+		break;
+	case "February": 
+		console.log("Winter");
+		break;
+	case "March": 
+		console.log("Spring");
+		break;
+	case "April": 
+		console.log("Spring");
+		break;
+    case "May": 
+		console.log("Spring");
+		break;
+    case "June": 
+		console.log("Summer");
+		break;
+    case "July": 
+		console.log("Summer");
+		break;
+    case "August": 
+		console.log("Summer");
+		break;
+    case "September": 
+		console.log("Fall");
+		break;
+    case "October": 
+		console.log("Fall");
+		break;
+    case "November": 
+		console.log("Fall");
+		break;
+    case "December": 
+		console.log("Winter");
+		break;
+	default: 
+		console.log("That's not a month...");
+}
